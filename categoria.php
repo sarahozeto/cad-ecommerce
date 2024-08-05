7c3e50d69dc622cbbd8897e74803a1aaa46fa9b3
ozztosarah <127869612+sarahozeto@users.noreply.github.com>
2024-08-05 09:31:22 -0300

    Add files via upload

---
+++ categoria.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Cadastrar categoria</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/style.css'>
+</head>
+<body>
+    <header> 
+        <div class="center">
+            <h1>Cadastro de Categoria</h1>
+            <a href="index.php" target="_self"> Voltar </a>
+        </div>
+    </header>
+        <section id="produtos">
+            <form action="insere-categoria.php" method="post">
+                <label for="">Descrição</label>
+                <input type="text" name="descricao">
+                <input type="submit" value="Cadastrar">
+            </form>
+        </section>
+</body>
+</html>
